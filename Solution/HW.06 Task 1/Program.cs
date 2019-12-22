9be5a6492bebe84d70fd0950af55caad278142d7
Lera Savchenko <lerasavchenk@gmail.com>
2019-12-22 22:25:17 +0300

    Completed task 1 from HW 6.

---
+++ Solution/HW.06 Task 1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HW._06_Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] array1 = new int[3];
+            Random value = new Random();
+            for(int i = 0; i < array1.Length; i++)
+            {
+                array1[i] = value.Next();
+                Console.WriteLine(array1[i]);
+            }
+
+            int[] array2 = new int[] { 5, 11, 7 };
+            foreach(int i in array2)
+            {
+                Console.WriteLine(i);
+            }
+
+            int[] array3 = new int[3];
+            for(int i = 0; i < array3.Length; i++)
+            {
+                array3[i] = array1[i] + array2[i];
+                Console.WriteLine(array3[i]);
+            } 
+            Console.ReadLine();
+
+            
+        }
+    }
+}
