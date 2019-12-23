284828d4505e705716e9cf37edda1bcf1993aff3
Lera Savchenko <lerasavchenk@gmail.com>
2019-12-24 00:13:51 +0300

    began task 2 of HW06.

--- Solution/HW.06 Task 2/Program.cs
+++ Solution/HW.06 Task 2/Program.cs
@@ -6,7 +6,29 @@ namespace HW._06_Task_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] array = new int[5];
+            array[0] = 1;
+            array[1] = 2;
+            array[2] = 3;
+            array[3] = 4;
+            
+            for( int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            Console.WriteLine();
+
+            array.SetValue(array.GetValue(0), 3);
+
+            /*int[] array2 = new int[5];
+            Array.Copy(array, 3, array2, 3, array.Length);
+
+            for(int i = 0; i < array2.Length; i++)
+            {
+                Console.WriteLine(array2[i]);*/
+            }
+            Console.ReadLine();
+
         }
     }
 }
