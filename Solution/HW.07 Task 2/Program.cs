74c74769e3027b3671b86cafd6321dea278c57de
Lera Savchenko <lerasavchenk@gmail.com>
2019-12-29 17:33:40 +0300

    Completed SubTask1 of Task 2.

---
+++ Solution/HW.07 Task 2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace HW._07_Task_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter a string");
+            string enteredString = Console.ReadLine();
+            string[] stringArray = enteredString.Split(' ');
+            
+            SubTask1(stringArray);
+          
+        }
+
+        static void SubTask1 (string[] str)
+        {
+            int maxLength = int.MinValue;
+            int index = new int();
+
+            Console.WriteLine("Final string: ");
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if(maxLength < str[i].Length)
+                {
+                    maxLength = (int)str[i].Length;
+                    index = i;
+                }
+            }
+            str[index] = string.Empty;
+        }
+
+        static void SubTask2(string[] str)
+        {
+
+        }
+    }
+}
