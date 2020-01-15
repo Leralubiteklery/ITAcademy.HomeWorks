0d30ee0c21afcd53dc8dd01921cb3569d7fb3344
Lera Savchenko <lerasavchenk@gmail.com>
2020-01-15 23:24:22 +0300

    Created Welcome method.

---
+++ Solution/HW.09/Passenger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW._09
+{
+    class Passenger
+    {
+        public string PassengerName { get; set; }
+        public string FlightNumber { get; set; }
+
+        public Passenger(string name, string flightNumber)
+        {
+            PassengerName = name;
+            FlightNumber = flightNumber;
+        }
+
+
+       
+
+    }
+}
---
+++ Solution/HW.09/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HW._09
+{
+    class Program
+    {
+        public void Main(string[] args)
+        {
+            WelcomePassenger();
+
+        }
+
+        public void WelcomePassenger()
+        {
+            const string AirportName = "Domodedovo";
+
+            Console.WriteLine($"Welcome to {AirportName} airport! Please, introduce yourself.");
+
+            Passenger passenger = new Passenger(Console.ReadLine(), Console.ReadLine());
+
+            Console.WriteLine($"Hello, {passenger.PassengerName}! Please, enter your flight number");
+            Console.ReadLine();
+            Console.WriteLine("Please, proceed to check-in counter");
+        }
+    }
+}
