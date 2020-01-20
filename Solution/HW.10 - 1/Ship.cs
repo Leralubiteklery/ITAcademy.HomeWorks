16d2ec93d0296a7334e7da87a9e747568ca17f4e
Lera Savchenko <lerasavchenk@gmail.com>
2020-01-20 23:10:21 +0300

    Completed task 1.

---
+++ Solution/HW.10 - 1/Ship.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW._10___1
+{
+    public class Ship
+    {
+        public int Draft;
+        public int Crew;
+
+        public Ship(int draft, int crew)
+        {
+            Draft = draft;
+            Crew = crew;
+        }
+
+        public bool IsWorthIt(int draft, int crew)
+        {
+            double crewWeight = crew * 1.5;
+            double draftWithoutCrew = draft - crewWeight;
+
+            if(draftWithoutCrew > 20)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
