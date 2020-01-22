5dc00dcbb95adc1ea7a27b635eaaace9dc327c01
Lera Savchenko <lerasavchenk@gmail.com>
2020-01-22 21:32:29 +0300

    Completed task 3.

---
+++ Solution/HW.10 - 3/Ball.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW._10___3
+{
+    class Ball
+    {
+        public string ballType { get; set; }
+
+        public Ball()
+        {
+            ballType = "regular";
+        }
+
+        public Ball(string ballType)
+        {
+            this.ballType = ballType;
+        }
+    }
+}
