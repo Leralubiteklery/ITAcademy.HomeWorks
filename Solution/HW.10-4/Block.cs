0774e8ee95767b5bcb950e372ff8c941b5168670
Lera Savchenko <lerasavchenk@gmail.com>
2020-01-22 21:36:50 +0300

    Completed Task 4.

---
+++ Solution/HW.10-4/Block.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW._10_4
+{
+    class Block
+    {
+        private int[] block = new int[3];
+
+        public Block(int[] block)
+        {
+            this.block = block;
+        }
+
+        public int GetWidth()
+        {
+            return block[0];
+        }
+        public int GetLength()
+        {
+            return block[1];
+        }
+
+        public int GetHeight()
+        {
+            return block[2];
+        }
+
+        public int GetVolume()
+        {
+            return block[0] * block[1] * block[2];
+        }
+
+        public int GetSurfaceArea()
+        {
+            int multiplier = 1;
+
+            foreach (int i in block)
+            {
+                multiplier = i * i + 1;
+            }
+            return multiplier * 2;
+        }
+    }
+}
---
+++ Solution/HW.10-4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HW._10_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        /* https://www.codewars.com/kata/55b75fcf67e558d3750000a3/train/csharp
+         * Write a class Block that creates a block (Duh..)
+
+        ##Requirements
+
+        The constructor should take an array as an argument, 
+        this will contain 3 integers of the form [width, length, height] fr
+        om which the Block should be created. */
+
+
+        }
+    }
+}
