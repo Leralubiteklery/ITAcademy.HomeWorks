f2038db1b80099daa106caf9cda11b94cd65f233
Lera Savchenko <lerasavchenk@gmail.com>
2020-01-22 21:38:50 +0300

    Completed Task 5.

---
+++ Solution/HW.10 - 5/Kata.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW._10___5
+{
+    class Kata
+    {
+        public static int[] Remove(int[] integerList, int[] valuesList)
+        {
+            int count = integerList.Length;
+
+            for (int i = 0; i < integerList.Length; i++)
+            {
+                for (int j = 0; j < valuesList.Length; j++)
+                {
+                    if (integerList[i].Equals(valuesList[j]))
+                    {
+                        integerList[i] = 0;
+                        count--;
+                    }
+                }
+            }
+
+            int[] updatedList = new int[count];
+
+            count = 0;
+            for (int i = 0; i < integerList.Length; i++)
+            {
+                if (integerList[i] != 0)
+                {
+                    updatedList[count] = integerList[i];
+                    count++;
+                }
+            }
+            return updatedList;
+        }
+    }
+}
---
+++ Solution/HW.10 - 5/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HW._10___5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* https://www.codewars.com/kata/563089b9b7be03472d00002b
+             * Define a method/function that removes from a given array 
+             * of integers all the values contained in a second array.*/
+        }
+    }
+}
