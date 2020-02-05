36e09805534ba4e38404b36b8185a727fc907d68
Lera Savchenko <lerasavchenk@gmail.com>
2020-02-05 18:00:06 +0300

    Created Moto class. Added some CRUD methods

---
+++ Solution/HW.12/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HW._12
+{
+    class Program
+    {
+        public static List<Moto> motorcycles = new List<Moto> { };
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
