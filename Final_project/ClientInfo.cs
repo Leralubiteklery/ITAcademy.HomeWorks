3a61cbc17e76f2dca1a1b268ff18c205f6931f26
Lera Savchenko <lerasavchenk@gmail.com>
2020-02-11 22:16:41 +0300

    Added menu.

---
+++ Final_project/ClientInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Final_project
+{
+    class ClientInfo
+    {
+
+    }
+}
---
+++ Final_project/Sushi.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Final_project
+{
+        class Sushi
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+            public double Price { get; set; }
+
+            public Sushi()
+            {
+            }
+            public Sushi(int id, string name, double price)
+            {
+                ID = id;
+                Name = name;
+                Price = price;
+            }
+        }
+}
+    
